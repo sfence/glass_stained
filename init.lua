b5631ad8f7ec0ee8f232f3633d5db4c86b90ea2c
SFENCE <sfence.software@gmail.com>
2021-05-06 06:31:14 +0200

    Update for compatibility with Hades Revisited.

--- init.lua
+++ init.lua
@@ -67,10 +67,10 @@ local panes = {
 		"Glass Pane",
 		"default_glass.png",
 		"glass_stained_edge.png",
-		default.node_sound_glass_defaults(),
+		hades_sounds.node_sound_glass_defaults(),
 		{
-			{"xpanes:pane_flat", "xpanes:pane_flat"},
-			{"xpanes:pane_flat", "xpanes:pane_flat"}
+			{"hades_xpanes:pane_flat", "hades_xpanes:pane_flat"},
+			{"hades_xpanes:pane_flat", "hades_xpanes:pane_flat"}
 		},
 		"4"
 	},
@@ -79,10 +79,10 @@ local panes = {
 		"Obsidian Glass Pane",
 		"default_obsidian_glass.png",
 		"xpanes_edge_obsidian.png",
-		default.node_sound_glass_defaults(),
+		hades_sounds.node_sound_glass_defaults(),
 		{
-			{"xpanes:obsidian_pane_flat", "xpanes:obsidian_pane_flat"},
-			{"xpanes:obsidian_pane_flat", "xpanes:obsidian_pane_flat"}
+			{"hades_xpanes:obsidian_pane_flat", "hades_xpanes:obsidian_pane_flat"},
+			{"hades_xpanes:obsidian_pane_flat", "hades_xpanes:obsidian_pane_flat"}
 		},
 		"4"
 	},
@@ -91,10 +91,10 @@ local panes = {
 		"Steel Bars",
 		"xpanes_bar.png",
 		"xpanes_bar_top.png",
-		default.node_sound_metal_defaults(),
+		hades_sounds.node_sound_metal_defaults(),
 		{
-			{"xpanes:bar_flat", "xpanes:bar_flat"},
-			{"xpanes:bar_flat", "xpanes:bar_flat"}
+			{"hades_xpanes:bar_flat", "hades_xpanes:bar_flat"},
+			{"hades_xpanes:bar_flat", "hades_xpanes:bar_flat"}
 		},
 		"4"
 	},
@@ -186,7 +186,7 @@ end
 
 for name, selection_box in pairs(selection_boxes) do
 	for node = 1, 18 do
-		minetest.register_node("glass_stained:glass_"..node.."_"..name, {
+		minetest.register_node("hades_glass_stained:glass_"..node.."_"..name, {
 			description = "Stained Glass "..node.." ("..name:sub(1, 1):upper()..name:sub(2, -1)..")",
 			drawtype = "nodebox",
 			tiles = {"glass_stained_"..node..".png"},
@@ -205,25 +205,25 @@ for name, selection_box in pairs(selection_boxes) do
 			},
 			groups = {cracky = 3, oddly_breakable_by_hand = 3},
 			use_texture_alpha = true,
-			sounds = default.node_sound_glass_defaults()
+			sounds = hades_sounds.node_sound_glass_defaults()
 		})
 		
 		if name == "noncuple" then
-			define_crafts("glass_stained:glass_"..node, {
-				{"dye:"..crafting[node][1], "xpanes:pane_flat"},
-				{"dye:"..crafting[node][2], "xpanes:pane_flat"},
-				{"dye:"..crafting[node][3], "xpanes:pane_flat"}
+			define_crafts("hades_glass_stained:glass_"..node, {
+				{"dye:"..crafting[node][1], "hades_xpanes:pane_flat"},
+				{"dye:"..crafting[node][2], "hades_xpanes:pane_flat"},
+				{"dye:"..crafting[node][3], "hades_xpanes:pane_flat"}
 			}, "3")
 		end
 		
 		if node <= 12 then
-			minetest.register_alias("glass_stained:glass_number_"..alias[node], "glass_stained:glass_"..node.."single")
-			minetest.register_alias("glass_stained:glass_number_"..alias[node].."top", "glass_stained:glass_"..node.."double")
+			minetest.register_alias("hades_glass_stained:glass_number_"..alias[node], "hades_glass_stained:glass_"..node.."single")
+			minetest.register_alias("hades_glass_stained:glass_number_"..alias[node].."top", "hades_glass_stained:glass_"..node.."double")
 		end
 	end
 	
 	for _, pane in ipairs(panes) do
-		minetest.register_node("glass_stained:pane_"..pane[1].."_"..name, {
+		minetest.register_node("hades_glass_stained:pane_"..pane[1].."_"..name, {
 			description = pane[2].." ("..name:sub(1, 1):upper()..name:sub(2, -1)..")",
 			drawtype = "nodebox",
 			tiles = {pane[4], pane[4], pane[3]},
@@ -245,18 +245,18 @@ for name, selection_box in pairs(selection_boxes) do
 		})
 		
 		if name == "noncuple" then
-			define_crafts("glass_stained:pane_"..pane[1], pane[6], pane[7])
+			define_crafts("hades_glass_stained:pane_"..pane[1], pane[6], pane[7])
 		end
 	end
 end
 
-minetest.register_alias("glass_stained:glass_normal", "glass_stained:pane_glass_single")
-minetest.register_alias("glass_stained:glass_normal_top", "glass_stained:pane_glass_double")
-minetest.register_alias("glass_stained:obsidian_glass_normal", "glass_stained:pane_obsidian_glass_single")
-minetest.register_alias("glass_stained:obsidian_glass_normal_top", "glass_stained:pane_obsidian_glass_double")
-minetest.register_alias("glass_stained:steel_bars_normal", "glass_stained:pane_bar_single")
-minetest.register_alias("glass_stained:steel_bars_normal_top", "glass_stained:pane_bar_double")
-minetest.register_alias("glass_stained:steel_bars_fancy", "glass_stained:pane_bar_top_pane_single")
+minetest.register_alias("hades_glass_stained:glass_normal", "hades_glass_stained:pane_glass_single")
+minetest.register_alias("hades_glass_stained:glass_normal_top", "hades_glass_stained:pane_glass_double")
+minetest.register_alias("hades_glass_stained:obsidian_glass_normal", "hades_glass_stained:pane_obsidian_glass_single")
+minetest.register_alias("hades_glass_stained:obsidian_glass_normal_top", "hades_glass_stained:pane_obsidian_glass_double")
+minetest.register_alias("hades_glass_stained:steel_bars_normal", "hades_glass_stained:pane_bar_single")
+minetest.register_alias("hades_glass_stained:steel_bars_normal_top", "hades_glass_stained:pane_bar_double")
+minetest.register_alias("hades_glass_stained:steel_bars_fancy", "hades_glass_stained:pane_bar_top_pane_single")
 
 xpanes.register_pane("bar_top", {
 	description = "Spiked Steel Railing",
@@ -264,7 +264,7 @@ xpanes.register_pane("bar_top", {
 	inventory_image = "glass_stained_bar_fancy.png",
 	wield_image = "glass_stained_bar_fancy.png",
 	groups = {cracky = 2},
-	sounds = default.node_sound_metal_defaults(),
+	sounds = hades_sounds.node_sound_metal_defaults(),
 	recipe = {
 		{"", "default:steel_ingot", ""},
 		{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
@@ -277,7 +277,7 @@ xpanes.register_pane("pane", {
 	textures = {"default_glass.png","xpanes_pane_half.png","glass_stained_edge.png"},
 	inventory_image = "default_glass.png",
 	wield_image = "default_glass.png",
-	sounds = default.node_sound_glass_defaults(),
+	sounds = hades_sounds.node_sound_glass_defaults(),
 	groups = {snappy = 2, cracky = 3, oddly_breakable_by_hand = 3},
 	recipe = {
 		{"default:glass", "default:glass", "default:glass"},
@@ -290,7 +290,7 @@ xpanes.register_pane("obsidian_pane", {
 	textures = {"default_obsidian_glass.png","xpanes_pane_half.png","xpanes_edge_obsidian.png"},
 	inventory_image = "default_obsidian_glass.png",
 	wield_image = "default_obsidian_glass.png",
-	sounds = default.node_sound_glass_defaults(),
+	sounds = hades_sounds.node_sound_glass_defaults(),
 	groups = {snappy = 2, cracky = 3},
 	recipe = {
 		{"default:obsidian_glass", "default:obsidian_glass", "default:obsidian_glass"},
@@ -298,7 +298,7 @@ xpanes.register_pane("obsidian_pane", {
 	}
 })
 
-minetest.register_node("glass_stained:pane_bar_top_pane_single", {
+minetest.register_node("hades_glass_stained:pane_bar_top_pane_single", {
 	description = "Spiked Steel Railing Pane (Single)",
 	drawtype = "nodebox",
 	tiles = {"blank.png", "blank.png", "glass_stained_bar_fancy.png"},
@@ -316,10 +316,10 @@ minetest.register_node("glass_stained:pane_bar_top_pane_single", {
 		fixed = selection_boxes["single"],
 	},
 	groups = {cracky = 3, oddly_breakable_by_hand = 3},
-	sounds = default.node_sound_metal_defaults(),
+	sounds = hades_sounds.node_sound_metal_defaults(),
 })
 
-minetest.register_node("glass_stained:pane_bar_top_pane_offset", {
+minetest.register_node("hades_glass_stained:pane_bar_top_pane_offset", {
 	description = "Spiked Steel Railing Pane (Offset)",
 	drawtype = "nodebox",
 	tiles = {"blank.png", "blank.png", "glass_stained_bar_fancy.png"},
@@ -337,26 +337,26 @@ minetest.register_node("glass_stained:pane_bar_top_pane_offset", {
 		fixed = selection_boxes["offset"],
 	},
 	groups = {cracky = 3, oddly_breakable_by_hand = 3},
-	sounds = default.node_sound_metal_defaults(),
+	sounds = hades_sounds.node_sound_metal_defaults(),
 })
 
 minetest.register_craft({
-	output = "glass_stained:pane_bar_top_pane_single",
+	output = "hades_glass_stained:pane_bar_top_pane_single",
 	recipe = {
-		{"xpanes:bar_top_flat"}
+		{"hades_xpanes:bar_top_flat"}
 	},
 })
 
 minetest.register_craft({
-	output = "glass_stained:pane_bar_top_pane_offset",
+	output = "hades_glass_stained:pane_bar_top_pane_offset",
 	recipe = {
-		{"glass_stained:pane_bar_top_pane_single"}
+		{"hades_glass_stained:pane_bar_top_pane_single"}
 	},
 })
 
 minetest.register_craft({
-	output = "glass_stained:pane_bar_top_pane_single",
+	output = "hades_glass_stained:pane_bar_top_pane_single",
 	recipe = {
-		{"glass_stained:pane_bar_top_pane_offset"}
+		{"hades_glass_stained:pane_bar_top_pane_offset"}
 	},
 })
